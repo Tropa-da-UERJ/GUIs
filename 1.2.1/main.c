33a31666f7848f147c43496093cc2fdf87ac09c8
Chubisco <78450127+Chubisco@users.noreply.github.com>
2025-08-19 10:08:36 -0300

    Update main.c

--- 1.2.1/main.c
+++ 1.2.1/main.c
@@ -1,1 +1,36 @@
+#include <SDL2/SDL.h>
 
+int main (int argc, char* args[])
+{
+    /* INICIALIZACAO */
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window* win = SDL_CreateWindow("Desenho 1.2.1!",
+                         SDL_WINDOWPOS_UNDEFINED,
+                         SDL_WINDOWPOS_UNDEFINED,
+                         700, 700, SDL_WINDOW_SHOWN
+                      );
+    SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
+
+    /* EXECUÇÃO */
+    SDL_SetRenderDrawColor(ren, 0xff,0xff,0xFF,0x00);
+    SDL_RenderClear(ren);
+    SDL_SetRenderDrawColor(ren, 0x46,0x63,0x3a,0x00);
+    SDL_Rect r = { 40,20, 5,200 };
+    SDL_RenderFillRect(ren, &r);
+    SDL_SetRenderDrawColor(ren, 0x23,0xaa,0xff,0x00);
+    SDL_Rect r2 = { 400,200, 30,400 };
+    SDL_RenderFillRect(ren, &r2);
+    SDL_SetRenderDrawColor(ren, 0x53,0x27,0xdd,0x00);
+    SDL_Rect r3 = { 150,150, 200,5 };
+    SDL_RenderFillRect(ren, &r3);
+    SDL_SetRenderDrawColor(ren, 0x13,0x17,0x22,0x00);
+    SDL_Rect r4 = { 500,600, 100,70 };
+    SDL_RenderFillRect(ren, &r4);
+    SDL_RenderPresent(ren);
+    SDL_Delay(5000);
+
+    /* FINALIZACAO */
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+}
